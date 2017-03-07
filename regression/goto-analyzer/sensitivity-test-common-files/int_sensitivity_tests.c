b92e263a7944f0dd97eea12ce4dc8801dbd0a35d
Owen Jones <owen.jones@diffblue.com>
2017-03-07 17:13:31 +0000

    Improve tests for int

--- regression/goto-analyzer/sensitivity-test-common-files/int_sensitivity_tests.c
+++ regression/goto-analyzer/sensitivity-test-common-files/int_sensitivity_tests.c
@@ -2,28 +2,39 @@
 
 int main(int argc, char *argv[])
 {
-  // Test if we can represent constant ints, and also that the transformers are
+  // Test how well we can represent ints, and also that the transformers are
   // working correctly.
   int x=0;
+  int y=0;
+  if(argc>2)
+  {
+    y=1;
+  }
   assert(x==0);
   assert(x==1);
-  assert(x==argc);
+  assert(x==y);
 
   assert(x<1);
   assert(x<-1);
-  assert(x<argc);
+  assert(x<y);
   
   assert(x>-1);
   assert(x>1);
-  assert(x>argc);
+  assert(x>y);
   
   assert(x!=1);
   assert(x!=0);
-  assert(x!=argc);
+  assert(x!=y);
   
   assert(!(x==1));
   assert(!(x==0));
-  assert(!(x==argc));
+  assert(!(x==y));
   
+  // Test how well we can represent an int when it has more than one possible
+  // value
+  assert(y<2);
+  assert(y>2);
+  assert(y==1);
+
   return 0;
 }
