1165ddec8b23ae0ae02cc44c1403c5c79ab94490
thk123 <thomas@diffblue.com>
2017-11-01 16:59:17 +0000

    Renamed find declaration method
    To be more consistent, renamed to require rather than find since it
    throws if the declaration is not found

--- unit/testing-utils/require_goto_statements.h
+++ unit/testing-utils/require_goto_statements.h
@@ -19,35 +19,36 @@
 #ifndef CPROVER_TESTING_UTILS_CHECK_GOTO_FUNCTIONS_H
 #define CPROVER_TESTING_UTILS_CHECK_GOTO_FUNCTIONS_H
 
-class no_decl_found_exception : public std::exception
-{
-public:
-  explicit no_decl_found_exception(const std::string &varname) : _varname
-                                                               (varname) {}
-
-  virtual const char *what() const throw() {
-    std::ostringstream stringStream;
-    stringStream << "Failed to find declaration for: "
-                 << _varname;
-    return stringStream.str().c_str();
-  }
-
-private:
-  std::string _varname;
-};
 
 namespace require_goto_statements
 {
-  std::vector<code_assignt> find_struct_component_assignments(
-    const std::vector<codet> &statements,
-    const irep_idt &structure_name,
-    const irep_idt &component_name);
-
   struct pointer_assignment_locationt
   {
     optionalt<code_assignt> null_assignment;
     std::vector<code_assignt> non_null_assignments;
   };
+
+  class no_decl_found_exception : public std::exception
+  {
+  public:
+    explicit no_decl_found_exception(const std::string &varname) : _varname
+                                                                     (varname) {}
+
+    virtual const char *what() const throw() {
+      std::ostringstream stringStream;
+      stringStream << "Failed to find declaration for: "
+                   << _varname;
+      return stringStream.str().c_str();
+    }
+
+  private:
+    std::string _varname;
+  };
+
+  std::vector<code_assignt> find_struct_component_assignments(
+    const std::vector<codet> &statements,
+    const irep_idt &structure_name,
+    const irep_idt &component_name);
 
   std::vector<codet> get_all_statements(const exprt &function_value);
 
@@ -55,7 +56,7 @@ namespace require_goto_statements
     const irep_idt &pointer_name,
     const std::vector<codet> &instructions);
 
-  const code_declt & find_declaration_by_name(
+  const code_declt &require_declaration_of_name(
     const irep_idt &variable_name,
     const std::vector<codet> &entry_point_instructions);
 }
