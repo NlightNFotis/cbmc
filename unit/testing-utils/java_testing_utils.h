07fc72d6b08edda69ead87d074874361ce8e2769
thk123 <thomas@diffblue.com>
2017-11-01 16:37:55 +0000

    Moved functions into a namespace and documented them
    Also tightened up the typess of returns and inputs.
    
    Also refactored get_all_statements to just take the root of the tree,
    rather than a vector of roots.

--- unit/testing-utils/java_testing_utils.h
+++ unit/testing-utils/java_testing_utils.h
@@ -36,26 +36,28 @@ private:
   std::string _varname;
 };
 
-std::vector<code_assignt> find_struct_component_assignments(
-  const std::vector<codet> &statements,
-  const irep_idt &structure_name,
-  const irep_idt &component_name);
-
-struct pointer_assignment_locationt
+namespace require_goto_statements
 {
-  optionalt<code_assignt> null_assignment;
-  std::vector<code_assignt> non_null_assignments;
-};
+  std::vector<code_assignt> find_struct_component_assignments(
+    const std::vector<codet> &statements,
+    const irep_idt &structure_name,
+    const irep_idt &component_name);
 
-std::vector<codet> get_all_statements(
-  const exprt::operandst &instructions);
+  struct pointer_assignment_locationt
+  {
+    optionalt<code_assignt> null_assignment;
+    std::vector<code_assignt> non_null_assignments;
+  };
 
-pointer_assignment_locationt find_pointer_assignments(
-  const irep_idt &pointer_name,
-  const std::vector<codet> &instructions);
+  std::vector<codet> get_all_statements(const exprt &function_value);
 
-const exprt &find_declaration_by_name(
-  const irep_idt &variable_name,
-  const std::vector<codet> &entry_point_instructions);
+  pointer_assignment_locationt find_pointer_assignments(
+    const irep_idt &pointer_name,
+    const std::vector<codet> &instructions);
+
+  const code_declt & find_declaration_by_name(
+    const irep_idt &variable_name,
+    const std::vector<codet> &entry_point_instructions);
+}
 
 #endif //TEST_GEN_SUPERBUILD_JAVA_TESTING_UTILS_H
