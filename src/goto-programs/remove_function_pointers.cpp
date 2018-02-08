2e189a92b71ac576ff1e17b8ec97b2d8b40a0958
Fotis Koutoulakis <fotis.koutoulakis@diffblue.com>
2018-02-08 10:05:37 +0000

    Moved the class definition into the header file.

--- src/goto-programs/remove_function_pointers.cpp
+++ src/goto-programs/remove_function_pointers.cpp
@@ -29,65 +29,14 @@ Author: Daniel Kroening, kroening@kroening.com
 #include "compute_called_functions.h"
 #include "remove_const_function_pointers.h"
 
-class remove_function_pointerst:public messaget
+void remove_function_pointerst::compute_address_taken_in_symbols(
+  std::set<irep_idt> &address_taken)
 {
-public:
-  remove_function_pointerst(
-    message_handlert &_message_handler,
-    symbol_tablet &_symbol_table,
-    bool _add_safety_assertion,
-    bool only_resolve_const_fps,
-    const goto_functionst &goto_functions);
+  const symbol_tablet &symbol_table=ns.get_symbol_table();
 
-  void operator()(goto_functionst &goto_functions);
-
-  bool remove_function_pointers(goto_programt &goto_program);
-
-protected:
-  const namespacet ns;
-  symbol_tablet &symbol_table;
-  bool add_safety_assertion;
-
-  // We can optionally halt the FP removal if we aren't able to use
-  // remove_const_function_pointerst to sucessfully narrow to a small
-  // subset of possible functions and just leave the function pointer
-  // as it is.
-  // This can be activated in goto-instrument using
-  // --remove-const-function-pointers instead of --remove-function-pointers
-  bool only_resolve_const_fps;
-
-  void remove_function_pointer(
-    goto_programt &goto_program,
-    goto_programt::targett target);
-
-  std::set<irep_idt> address_taken;
-
-  typedef std::map<irep_idt, code_typet> type_mapt;
-  type_mapt type_map;
-
-  bool is_type_compatible(
-    bool return_value_used,
-    const code_typet &call_type,
-    const code_typet &function_type);
-
-  bool arg_is_type_compatible(
-    const typet &call_type,
-    const typet &function_type);
-
-  void fix_argument_types(code_function_callt &function_call);
-  void fix_return_type(
-    code_function_callt &function_call,
-    goto_programt &dest);
-
-  void compute_address_taken_in_symbols(
-    std::set<irep_idt> &address_taken)
-  {
-    const symbol_tablet &symbol_table=ns.get_symbol_table();
-
-    for(const auto &s : symbol_table.symbols)
-      compute_address_taken_functions(s.second.value, address_taken);
-  }
-};
+  for(const auto &s : symbol_table.symbols)
+    compute_address_taken_functions(s.second.value, address_taken);
+}
 
 remove_function_pointerst::remove_function_pointerst(
   message_handlert &_message_handler,
